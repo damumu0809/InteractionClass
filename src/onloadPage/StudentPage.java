dd1e6d4479e1943461afa136ba836321ea0f5671
unknown <>
2016-05-20 21:07:21 +0800

    first commit

---
+++ src/onloadPage/StudentPage.java
@@ -0,0 +1,112 @@
+package onloadPage;
+
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import database.DB;
+/**
+ * Servlet implementation class StudentPage
+ */
+@WebServlet("/StudentPage")
+public class StudentPage extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public StudentPage() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html;charset=utf-8");
+		java.io.PrintWriter out = response.getWriter( );		
+		
+		//连接数据库
+		DB db = new DB();
+		String sqlSelect = "SELECT * FROM IssueWork";
+		
+		ResultSet rs1 = db.query2(sqlSelect);
+		
+		try {
+			int taskNum = 0; //第几次作业
+			String theme = null; //作业内容
+			boolean finish = false; //是否已提交
+			String href = null; //提交之后的链接
+			
+			JSONObject work;
+			List allWork = new ArrayList();
+			
+			while(rs1.next()){
+				taskNum = rs1.getInt("id");
+				theme = rs1.getString("theme");
+				
+				//查询是否提交该次作业
+				String sql = "SELECT * FROM homework WHERE taskNum=\""+taskNum+"\" AND owner='xiaomu'";
+				ResultSet rs2 = db.query2(sql);
+				if(rs2.next()){
+					//已提交
+					finish = true;
+					href = rs2.getString("href");
+				}else {
+					//未提交
+					finish = false;
+				}
+				work = new JSONObject();
+	    	    work.put("taskNum", taskNum);
+	    	    work.put("theme", theme);
+	    	    work.put("finish", finish);
+	    	    work.put("href", href);
+	    	    //out.println(work.toString());
+	    	    
+	    	    allWork.add(work);
+	    	    
+			}
+			
+			JSONObject message = new JSONObject();
+			message.put("list", allWork);
+			message.put("code", 0);
+			
+			out.println(message.toString());
+			
+		} catch (SQLException | JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+//		out.println("<form action='/class/UploadWork' method='post' enctype='multipart/form-data'>");
+//		out.println("<input type='file' name='file' id='file'/>");
+//		out.println("<input type='hidden' name='taskNum' value=\""+taskNum+"\" />");
+//		out.println("<input type='submit' value='提交' />");
+//		out.println("</form>");
+	}
+
+}
---
+++ src/onloadPage/TeacherPage.java
@@ -0,0 +1,75 @@
+package onloadPage;
+
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import database.DB;
+
+/**
+ * Servlet implementation class TeacherPage
+ */
+@WebServlet("/TeacherPage")
+public class TeacherPage extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TeacherPage() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html;charset=utf-8");
+		java.io.PrintWriter out = response.getWriter( );
+		
+		
+		out.println("<html>");
+        out.println("<head>");
+        out.println("<title>TeacherPage</title>");  
+        out.println("</head>");
+        out.println("<body>");
+        
+        //显示每次作业的所有提交文件
+        DB db = new DB();
+		String sqlSelect = "SELECT * FROM IssueWork";
+		ResultSet rs1 = db.query2(sqlSelect);
+		try {
+			while(rs1.next()){
+				out.println("第"+rs1.getInt("id")+"次作业"+"</br>"+rs1.getString("theme")+"</br>");
+				String sql = "SELECT * FROM homework WHERE taskNum="+rs1.getInt("id")+"";
+				ResultSet rs2 = db.query2(sql);
+				while(rs2.next()){
+					out.println("<a href="+ rs2.getString("href") +">"+rs2.getString("file_name")+"</a>"+"</br>");
+				}
+			}	
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		//发布作业添加到这里
+		
+	}
+
+}
